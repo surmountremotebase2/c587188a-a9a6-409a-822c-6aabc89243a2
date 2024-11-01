23679a9a8539e5e3ce7e5035a0864b4204ed6b9c
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-11-01 14:53:06 +0500

    User David Wang saved changes to main.py at 11/01/2024, 09:53:06 UTC time.

--- d0b06a80-db0e-4fa3-8c5d-4ee3df5c6d21/main.py
+++ d0b06a80-db0e-4fa3-8c5d-4ee3df5c6d21/main.py
@@ -5,7 +5,7 @@ from surmount.logging import log
 class TradingStrategy(Strategy):
     def __init__(self):
         # Define the tickers we will be trading
-        self.tickers = ["AAPL"] #, "MSFT", "NVDA", "AMD", "META", "AMZN", "GOOGL", "NFLX", "TSLA"]
+        self.tickers = ["AAPL", "MSFT"] #, "NVDA", "AMD", "META", "AMZN", "GOOGL", "NFLX", "TSLA"]
         # Initial allocation of $3000 across the assets, can be adjusted based on strategy requirements
         self.initial_investment = 5000
 
