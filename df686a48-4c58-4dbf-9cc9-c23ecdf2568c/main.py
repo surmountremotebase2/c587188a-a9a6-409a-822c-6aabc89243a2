52e6094f98a380089e6097d2160f0dc6b04b4de5
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-11-01 07:04:01 +0500

    User David Wang saved changes to main.py at 11/01/2024, 02:04:00 UTC time.

--- df686a48-4c58-4dbf-9cc9-c23ecdf2568c/main.py
+++ df686a48-4c58-4dbf-9cc9-c23ecdf2568c/main.py
@@ -5,7 +5,7 @@ from surmount.logging import log
 class TradingStrategy(Strategy):
 
     def __init__(self):
-        self.tickers = ["TSLA", "AAPL", "MSFT", "NVDA", "AMD", "META"]
+        self.tickers = ["AAPL", "MSFT", "NVDA", "AMD", "META"]
 
     @property
     def interval(self):
