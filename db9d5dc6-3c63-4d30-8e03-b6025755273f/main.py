0e79445a3302dcd829c3957260777ac24521c86c
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-11-03 01:30:49 +0500

    User David Wang saved changes to main.py at 11/02/2024, 20:30:49 UTC time.

--- db9d5dc6-3c63-4d30-8e03-b6025755273f/main.py
+++ db9d5dc6-3c63-4d30-8e03-b6025755273f/main.py
@@ -4,7 +4,8 @@ from .macd import MACD  # Import the MACD function from the macd module
 
 class TradingStrategy(Strategy):
     def __init__(self):
-        self.tickers = ["AAPL", "MSFT", "WMT", "JPM"]#, "NVDA", "AMD", "META"]  # Adjusted tickers as needed
+        self.tickers = ["AAPL", "MSFT", "WMT", "JPM", "NVDA", "AMD", "META", "TSLA"
+        "AMZN", "GOOGL","LMT","NOC", "GC", "BAC", "GS", "PFE", "JNJ","FDX","UNP"]  # Adjusted tickers as needed
         self.total_investment = 3000  # Total investment amount is $3,000
         self.investment_rate = 0.5  # Rate at which to invest (50%)
         self.initial_allocation = self.total_investment * self.investment_rate / len(self.tickers)  # Equal allocation per ticker
