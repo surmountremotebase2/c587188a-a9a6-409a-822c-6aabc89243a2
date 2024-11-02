d4fe99dd101b21c82e9e21597661deb67e6c310b
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-11-03 03:55:31 +0500

    User David Wang saved changes to main.py at 11/02/2024, 22:55:31 UTC time.

--- db9d5dc6-3c63-4d30-8e03-b6025755273f/main.py
+++ db9d5dc6-3c63-4d30-8e03-b6025755273f/main.py
@@ -67,10 +67,10 @@ class TradingStrategy(Strategy):
             current_value = holding_dict[ticker] * current_close
             liquidate_value = allocation_dict[ticker] * 1.05  # The value to compare against
 
-            if (current_signal > current_macd and current_rsi < 48) or
-               (current_ema21 > current_ema9 and current_rsi < 48) or
-               (current_rsi > 70) or
-               (current_adx < 18) or
+            if (current_signal > current_macd and current_rsi < 48) or \
+               (current_ema21 > current_ema9 and current_rsi < 48) or \
+               (current_rsi > 70) or \
+               (current_adx < 18) or \
                (current_close >= current_bb_upper):
                 if current_value > liquidate_value:  # Only liquidate if the current value is greater than the allocation
                     allocation_dict[ticker] = 0  # Liquidate the stock
