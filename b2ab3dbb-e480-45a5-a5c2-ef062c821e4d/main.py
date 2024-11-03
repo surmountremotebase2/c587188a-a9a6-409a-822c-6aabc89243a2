f3f23b0be4dc4e25e8a730edc566cbbe60b0ce4e
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-11-03 05:34:02 +0500

    User David Wang saved changes to main.py at 11/03/2024, 00:34:01 UTC time.

--- b2ab3dbb-e480-45a5-a5c2-ef062c821e4d/main.py
+++ b2ab3dbb-e480-45a5-a5c2-ef062c821e4d/main.py
@@ -5,7 +5,6 @@ from surmount.technical_indicators import RSI
 class TradingStrategy(Strategy):
     def __init__(self):
         self.tickers = ["TSLA", "AAPL", "MSFT", "NVDA", "AMD", "META", "AMZN", "GOOGL", "NFLX"]
-        self.initial_investment = 3000
 
     @property
     def assets(self):
@@ -26,7 +25,7 @@ class TradingStrategy(Strategy):
 
                 # Check buy condition: RSI(4) greater than 65
                 if rsi > 65:
-                    allocation_dict[ticker] = self.initial_investment / len(self.tickers)
+                    allocation_dict[ticker] = 1/ len(self.tickers)
 
                 # Check sell condition: RSI(4) falls below 45
                 elif rsi < 45:
