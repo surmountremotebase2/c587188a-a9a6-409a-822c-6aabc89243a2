2a5a3b806d21d507b051cfef7ef3d1b1275fb583
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-11-03 12:23:01 +0500

    User David Wang saved changes to main.py at 11/03/2024, 07:23:00 UTC time.

--- db9d5dc6-3c63-4d30-8e03-b6025755273f/main.py
+++ db9d5dc6-3c63-4d30-8e03-b6025755273f/main.py
@@ -6,12 +6,12 @@ class TradingStrategy(Strategy):
     def __init__(self):
         self.tickers = [
             "META", "AAPL", "AMZN", "NFLX", "GOOGL", 
-            "NVDA", "AMD", "TSLA", "PLTR" #, "PFE", "NIO", "BBD"
+            "NVDA", "AMD", "TSLA", "PLTR", "WMT"
         ]  # Adjusted tickers as needed
 
     @property
     def interval(self):
-        return "1day"
+        return "1hour"
 
     @property
     def assets(self):
