221521ca65ba9d01b39df0975c6908008681ff80
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-11-04 13:28:42 +0500

    User David Wang saved changes to main.py at 11/04/2024, 08:28:41 UTC time.

--- db9d5dc6-3c63-4d30-8e03-b6025755273f/main.py
+++ db9d5dc6-3c63-4d30-8e03-b6025755273f/main.py
@@ -5,8 +5,8 @@ from .macd import MACD  # Import the MACD function from the macd module
 class TradingStrategy(Strategy):
     def __init__(self):
         self.tickers = [
-            "META", "AAPL", "AMZN", "GOOGL", 
-            "NVDA", "AMD", "TSLA", "WMT" #, "PLTR"
+            "GOOGL", "AAPL"#, "META", "AMZN", 
+            #"NVDA", "AMD", "TSLA", "WMT", "PLTR"
         ]  # Adjusted tickers as needed
 
     @property
