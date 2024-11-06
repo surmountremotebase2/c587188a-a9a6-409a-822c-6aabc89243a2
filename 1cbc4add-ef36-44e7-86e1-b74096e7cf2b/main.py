b65de7fc9005160899dd9b27f8bde4d5aaebee5d
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-11-06 11:41:32 +0500

    User David Wang saved changes to main.py at 11/06/2024, 06:41:32 UTC time.

--- 1cbc4add-ef36-44e7-86e1-b74096e7cf2b/main.py
+++ 1cbc4add-ef36-44e7-86e1-b74096e7cf2b/main.py
@@ -6,9 +6,9 @@ from .macd import MACD
 class TradingStrategy(Strategy):
     def __init__(self):
         self.tickers = [
-            "META","AAPL", "AMZN", "NFLX", "GOOG", "PYPL", "TSLA", "NVDA", "MSFT", "NVDA", "AMD"
+            #"META","AAPL", "AMZN", "NFLX", "GOOG", "PYPL", "TSLA", "NVDA", "MSFT", "NVDA", "AMD"
             #"DIS", "HD", "WMT", "SQ", "BIIB","AVGO", "QCOM", "MU", "UNH", "SHOP", "AMGN"
-            #"PEP", "XOM", "PYPL", "JPM", "PG", "LMT", "NOC", "CRM", "DHR", "NOW", "QQQ", "SPY", "IWM"
+            "PEP", "XOM", "PYPL", "JPM", "PG", "LMT", "NOC", "CRM", "DHR", "NOW", "QQQ", "SPY", "V"
         ]
         self.holding_dict = {ticker: 0 for ticker in self.tickers}
         self.entry_prices = {ticker: 0 for ticker in self.tickers}  # Track entry prices for ATR-based stop loss
