85c739ee4883484d399230ad97e2b566ed9f48e2
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-11-06 08:47:03 +0500

    User David Wang saved changes to main.py at 11/06/2024, 03:47:02 UTC time.

--- 8a822f55-7577-498d-bbb3-4171067ff096/main.py
+++ 8a822f55-7577-498d-bbb3-4171067ff096/main.py
@@ -71,7 +71,7 @@ class TradingStrategy(Strategy):
             ])
 
             if current_adx > 24:
-                if buy_conditions_met >= 3:  # Buy if any three conditions are met
+                if buy_conditions_met >= 2:  # Buy if any three conditions are met
                     allocation_dict[ticker] += (buy_conditions_met / 6) * (2000 / len(self.tickers))  # Allocate dynamically based on conditions met
                     self.holding_dict[ticker] += allocation_dict[ticker] / current_close
                     self.entry_prices[ticker] = current_close
