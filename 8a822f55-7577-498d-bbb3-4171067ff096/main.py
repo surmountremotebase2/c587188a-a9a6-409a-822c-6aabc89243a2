3a87266ca56cc91d2d08a22c454ce3f15a67d8d1
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-11-06 13:05:52 +0500

    User David Wang saved changes to main.py at 11/06/2024, 08:05:51 UTC time.

--- 8a822f55-7577-498d-bbb3-4171067ff096/main.py
+++ 8a822f55-7577-498d-bbb3-4171067ff096/main.py
@@ -16,7 +16,7 @@ class TradingStrategy(Strategy):
 
     @property
     def interval(self):
-        return "1hour"  # Using 1-hour interval
+        return "1day  # Using 1-hour interval
 
     @property
     def assets(self):
