cbf6a2be7eb230266a5284e394981704e7b48ef0
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-11-06 13:00:54 +0500

    User David Wang saved changes to main.py at 11/06/2024, 08:00:53 UTC time.

--- 8a822f55-7577-498d-bbb3-4171067ff096/main.py
+++ 8a822f55-7577-498d-bbb3-4171067ff096/main.py
@@ -66,12 +66,12 @@ class TradingStrategy(Strategy):
 
             # Buy conditions (any two conditions met)
             buy_conditions_met = sum([
-                #current_ema9 > current_ema21 and current_rsi < 40,
-                current_ema9 > current_ema21 and current_rsi > 60,  # More aggressive bullish confirmation
-                #current_macd > current_signal and current_rsi < 40,  # RSI below 30 and MACD crosses above signal line
-                current_macd > current_signal and current_rsi > 60,  # MACD crosses above signal line and RSI > 55
-                current_close <= current_bb_lower and current_rsi > 50,  # Price below lower Bollinger Band
-                current_slope_value > 0 and current_momentum_value > 0 and current_rsi > 50  # Positive slope and increasing momentum
+                current_ema9 > current_ema21 and current_rsi < 45,
+                #current_ema9 > current_ema21 and current_rsi > 60,  # More aggressive bullish confirmation
+                current_macd > current_signal and current_rsi < 50,  # RSI below 30 and MACD crosses above signal line
+                #current_macd > current_signal and current_rsi > 60,  # MACD crosses above signal line and RSI > 55
+                current_close <= current_bb_lower and current_rsi < 40,  # Price below lower Bollinger Band
+                current_slope_value > 0 and current_momentum_value > 0 and current_rsi < 50  # Positive slope and increasing momentum
             ])
 
             if current_adx > 20 and current_ema9 > current_ema21 and current_macd > current_signal:
@@ -83,10 +83,10 @@ class TradingStrategy(Strategy):
             # Sell conditions (any two conditions met)
             sell_conditions_met = sum([
                 #current_ema21 > current_ema9 and current_rsi > 60,
-                current_ema21 > current_ema9 and current_rsi < 40,
+                current_ema21 > current_ema9 and current_rsi < 45,
                 #current_macd < current_signal and current_rsi > 70,  # RSI above 70 and MACD crosses below signal line
-                current_macd < current_signal and current_rsi < 40,  # MACD crosses below signal line and RSI < 45
-                current_close >= current_bb_upper and current_rsi > 70,  # Price above upper Bollinger Band and RSI > 70
+                current_macd < current_signal and current_rsi < 50,  # MACD crosses below signal line and RSI < 45
+                current_close >= current_bb_upper and current_rsi > 65,  # Price above upper Bollinger Band and RSI > 70
                 current_slope_value < 0 and current_momentum_value < 0 and current_rsi < 45  # Negative slope and weakening momentum
                 
             ])
