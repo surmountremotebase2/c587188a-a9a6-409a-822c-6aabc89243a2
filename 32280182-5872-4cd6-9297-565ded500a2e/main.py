1274b6465137e73c2ed76e6df895b3b26c91e358
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-11-09 04:00:17 -0500

    User David Wang saved changes to main.py at 11/09/2024, 09:00:16 UTC time.

--- 32280182-5872-4cd6-9297-565ded500a2e/main.py
+++ 32280182-5872-4cd6-9297-565ded500a2e/main.py
@@ -74,7 +74,7 @@ class TradingStrategy(Strategy):
 
                 # Sell Conditions
                 if macd_line and signal_line:
-                    if (current_signal > current_macd) and (previous_macd > previous_signal) and (current_signal - current_signal) > 0 and current_rsi > 65):  # Sell condition 1
+                    if (current_signal > current_macd) and (previous_macd > previous_signal) and (current_signal - current_macd) > 0 and current_rsi > 65):  # Sell condition 1
                         allocation_dict[ticker] = 0.0  # Sell the position
                         log(f"Sell signal for {ticker}: Signal > MACD: MACD={current_macd}, Signal={current_signal}, EMA9={current_ema9}, EMA21={current_ema21}, RSI={current_rsi}, MFI={current_mfi}, ADX={current_adx}, CCI={current_cci}, ATR={current_atr}")
 
